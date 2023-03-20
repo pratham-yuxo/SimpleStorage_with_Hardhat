ad14d93a95a4d0180112986dd9377057e6b23c70
pratham <darkvoid6565@gmail.com>
2023-03-20 13:37:58 +0530

    first commit

---
+++ hardhat.config.js
@@ -0,0 +1,10 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+
+// type yarn hardhat to know every command of hardhat
+
+// here you can specify the version of solidity on which u wanna work
+module.exports = {
+  solidity: "0.8.8",
+};
